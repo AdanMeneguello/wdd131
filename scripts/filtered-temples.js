0913d8e83e2d201b729cff3ae087ffad2ab11b88
Adan Meneguello <meneguelloadan@gmail.com>
2025-08-09 17:50:37 -0300

    Adding Filtered-temples files

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -100,34 +100,78 @@ const temples = [
   }
 ];
 
-createTempleCard(temples)
+function createTempleCard(temples) {
+    temples.forEach(temple => {
+        let card = document.createElement("section");
+        let name = document.createElement("h3");
+        let location = document.createElement("p");
+        let dedication = document.createElement("p");
+        let area = document.createElement("p");
+        let img = document.createElement("img");
+        let figure = document.createElement("figure");
 
-function createTempleCard() {
-  temples.forEach(temple => {
-    let card = document.createElement("section");
-    let name = document.createElement("h3");
-    let location = document.createElement("p");
-    let dedication = document.createElement("p");
-    let area = document.createElement("p");
-    let img = document.createElement("img");
+        name.textContent = temple.templeName;
+        location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
+        dedication.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
+        area.innerHTML = `<span class="label">Size:</span> ${temple.area} sq ft`;
 
-    name.textContent = temple.templeName;
-    location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
-    dedication.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
-    area.innerHTML = `<span class="label">Size:</span> ${temple.area} sq ft`;
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", `${temple.templeName} Temple`);
+        img.setAttribute("loading", "lazy");
 
-    img.setAttribute("src", temple.imageUrl);
-    img.setAttribute("alt", `${temple.templeName} Temple`);
-    img.setAttribute("loading", "lazy");
+        figure.appendChild(img);
 
-    card.appendChild(name);
-    card.appendChild(location);
-    card.appendChild(dedication);
-    card.appendChild(area);
-    card.appendChild(img);
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+        card.appendChild(figure);
 
-    document.querySelector(".res-grid").appendChild(card);
+        document.querySelector(".gallery").appendChild(card);
   });
 }
 
-createTempleCard();
+
+function clearGallery() {
+    document.querySelector(".gallery").innerHTML = "";
+}
+
+function filterTemples(filter) {
+    let filtered = [];
+    temples.forEach(t => {
+        const year = parseInt(t.dedicated.split(",")[0]);
+        if (filter === "Old" && year < 1900) {
+            filtered.push(t);
+        }
+        else if (filter === "New" && year > 2000) {
+            filtered.push(t);
+        } 
+        else if (filter === "Large" && t.area > 90000) {
+            filtered.push(t);
+        }
+        else if (filter === "Small" && t.area < 10000) {
+            filtered.push(t);
+        } 
+        else if (filter === "Home") {
+            filtered = temples;
+        }
+    });
+    if (filter === "Home") {
+        filtered = temples;
+    } 
+    clearGallery();
+    createTempleCard(filtered);
+}
+
+document.querySelectorAll("#nav-menu a").forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const filter = link.textContent.trim();
+        filterTemples(filter);
+    });
+});
+clearGallery();
+
+
+createTempleCard(temples);
